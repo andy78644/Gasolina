ae1df6c204c24d609c0fa4f025d68f84c0fd5de8
Steffen Wirth <steffen.wirth@pushtechniques.com>
2023-12-11 15:02:57 +0800

    add worker for sui/mina

--- src/mina.py
+++ src/mina.py
@@ -17,8 +17,13 @@ load_dotenv(dotenv_path)
 import pymysql
 db = pymysql.connect(host=os.environ.get('DATABASE_HOST'),user=os.environ.get('DATABASE_USER'),password=os.environ.get('DATABASE_PASSWORD'),database=os.environ.get('DATABASE_DB'),cursorclass=pymysql.cursors.DictCursor)
 
-
 # connect to websocket
 # receive latest blocks
 # store information in database
+
+def main():
+  print("Hello World!")
+
+if __name__ == "__main__":
+  main()
 
